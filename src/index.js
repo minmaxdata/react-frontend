a77a86824a7b7ca03462d5a7756073091b15c04d
minmaxdata <mcdreamer60@gmail.com>
2018-05-31 11:23:38 -0700

    edits to add dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
-//import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 //import reducer from './reducers'
-//import { Provider } from 'react-redux'
+import App from './containers/App'
+import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import 'normalize.css'
 
 
+// const middleware = [ thunk ]
+// if (process.env.NODE_ENV !== 'production') {
+//   middleware.push(createLogger())
+// }
+//
+// const store = createStore(
+//   reducer,
+//   applyMiddleware(...middleware)
+// )
+
+
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+
+    <BrowserRouter><App /></BrowserRouter>,
+
   document.getElementById('root')
 );
 registerServiceWorker();
