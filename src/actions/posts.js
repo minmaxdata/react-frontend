c80cfd6f5faaf4fffbbb47f7f45146c5ab5b8c51
minmaxdata <mcdreamer60@gmail.com>
2018-06-20 12:26:01 -0700

    getting posts by categories redux

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -7,6 +7,12 @@ export const getAllPosts = () => {
     return ReadableAPI.getPosts().then(response => dispatch(receiveAllPosts(response)));
   }
 };
+export const getPostsByCategory = category => {
+  return dispatch => {
+    dispatch(requestPostsByCategory())
+    return ReadableAPI.getPostsByCategory(category).then(response => dispatch(receivePostsByCategory(response)));
+  }
+};
 
 export const receiveAllPosts = posts => {
   return {
@@ -17,5 +23,17 @@ export const receiveAllPosts = posts => {
 export const requestAllPosts= () => {
   return {
     type: types.REQUEST_ALL_POSTS
+  }
+}
+
+export const receivePostsByCategory = posts => {
+  return {
+    type: types.RECEIVE_POSTS_BY_CATEGORY,
+    posts: posts
+  };
+};
+export const requestPostsByCategory = () => {
+  return {
+    type: types.REQUEST_POSTS_BY_CATEGORY
   }
 }
